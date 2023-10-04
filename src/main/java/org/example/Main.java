c2d8b1358564538fad1b2b26cd8d64940d0786bd
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-10-04 23:17:03 +0300

    give solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
+        Hello hello = new Hello();
+
+        System.out.println(hello.sayHello(new String[]{"John", "Doe"}, "Timisoara", "Timis"));
     }
 }
